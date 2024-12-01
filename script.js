3bf711e1b5e07414557fe17cdfc6d0f830e410d7
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2024-12-01 21:47:25 +0300

    Created Functions to count score and determine who wins

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-//SCORE VARIABLES
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  //SCORE VARIABLES
+  let humanScore = 0;
+  let computerScore = 0;
 
-// COMPUTER CHOICE FUNCTION
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  if (randomNumber === 0) {
-    return "Rock";
-  } else if (randomNumber === 1) {
-    return "Paper";
-  } else {
-    return "Scissors";
-  }
-}
-const computerSelection = getComputerChoice();
-
-//HUMAN CHOICE FUNCTION
-function getHumanChoice() {
-    let userInput = prompt("Enter your decision! Rock, Paper or Scissor?")
-    if (userInput.toLowerCase() === "rock") {
-        return "Rock"
-    } else if (userInput.toLowerCase() === "paper") {
-        return "Paper"
-    } else if (userInput.toLowerCase() === "scissors") {
-        return "Scissors"
-    }
-}
-const humanSelection = getHumanChoice();
-
-function playRound(humanChoice, computerChoice) {
-    if (humanSelection === "Paper" && computerSelection === "Paper") {
-        return "Draw! Paper can't beat Paper!"
-    } else if (humanSelection === "Rock" && computerSelection === "Rock") {
-        return "Draw! Rock can't beat Rock!"
-    } else if (humanSelection === "Scissors" && computerSelection === "Scissors") {
-        return "Draw! Scissors can't beat Scissors!"
+  // COMPUTER CHOICE FUNCTION
+  function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+      return "Rock";
+    } else if (randomNumber === 1) {
+      return "Paper";
     } else {
-        return "deneme"
+      return "Scissors";
     }
+  }
+  const computerSelection = getComputerChoice();
+
+  //HUMAN CHOICE FUNCTION
+  function getHumanChoice() {
+    let userInput = prompt("Enter your decision! Rock, Paper or Scissor?");
+    if (userInput.toLowerCase() === "rock") {
+      return "Rock";
+    } else if (userInput.toLowerCase() === "paper") {
+      return "Paper";
+    } else if (userInput.toLowerCase() === "scissors") {
+      return "Scissors";
+    }
+  }
+  const humanSelection = getHumanChoice();
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Rock") {
+      return "Draw! Rock can't beat Rock";
+    } else if (humanChoice === "Paper" && computerChoice === "Paper") {
+      return "Draw! Paper can't beat Paper";
+    } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
+      return "Draw! Paper can't beat Paper";
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+      return "You Lose! Rock can't beat Paper";
+    } else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+      return "You Win! Rock can beat Scissors";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+      return "You win! Paper beat Rock";
+    } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+      return "You Lose! Paper can't beat Scissors";
+    } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+      return "You Lose! Scissors can't beat Rock";
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+      return "You Win! Scissors can't beat Paper";
+    }
+  }
+
+  let result = playRound(humanSelection, computerSelection);
+  console.log(result);
 }
 
-
+for (let i = 0; i < 5; i++) {
+    playGame();
+}
